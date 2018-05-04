0e4063b79cd598a2e460d063461b630b73593062
Pasquale Fiorillo <me@pasqualefiorillo.it>
2018-05-04 22:52:03 +0200

    fix wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,7 @@
 <?php
+define('WP_HOME', $_SERVER['WP_HOME']);
+define('WP_SITEURL', $_SERVER['WP_SITEURL']);
+
 define('DB_NAME', $_SERVER['DB_NAME']);
 define('DB_USER', $_SERVER['DB_USER']);
 define('DB_PASSWORD', $_SERVER['DB_PASSWORD']);
